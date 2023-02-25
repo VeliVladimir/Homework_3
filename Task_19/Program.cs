66092aeff0ae19dd464be15a17eda76825347f70
VeliVladimir <124333630+VeliVladimir@users.noreply.github.com>
2023-02-25 14:20:59 +0300

    Update Program.cs

--- Task_19/Program.cs
+++ Task_19/Program.cs
@@ -7,7 +7,7 @@ Console.Clear();
 Console.Write ("Введите пятизначное число: ");
 string? number = Console.ReadLine();
 void CheckingNumber(string number){
-  if (number[0]==number[4] || number[1]==number[3]){
+  if (number[0]==number[4] && number[1]==number[3]){
     Console.WriteLine($"Ваше число: {number} - палиндром.");
   }
   else Console.WriteLine($"Ваше число: {number} - НЕ палиндром.");
